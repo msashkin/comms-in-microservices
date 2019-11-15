d111c156ade34558bfd6e9c74b6a7c95ca62f051
Maxim Sashkin <m@sashkin.net>
2019-11-15 22:33:59 +0200

    Add async support to controllers

--- task-1/web-service-1/src/main/java/com/msashkin/webservice/controller/WebServiceController.java
+++ task-1/web-service-1/src/main/java/com/msashkin/webservice/controller/WebServiceController.java
@@ -1,5 +1,7 @@
 package com.msashkin.webservice.controller;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.msashkin.rpchelloservice.service.HelloResponse;
 import com.msashkin.webservice.client.RpcHelloServiceClient;
@@ -7,12 +9,15 @@ import com.msashkin.webservice.config.WebServiceRpcOneConfigurationProperties;
 import com.msashkin.webservice.config.WebServiceRpcTwoConfigurationProperties;
 import com.msashkin.webservice.model.MessageResponse;
 import com.msashkin.webservice.model.MessagesResponse;
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 @RestController
 @RequestMapping("/api")
@@ -20,11 +25,14 @@ public class WebServiceController {
 
     private final WebServiceRpcOneConfigurationProperties rpcOneConfigurationProperties;
     private final WebServiceRpcTwoConfigurationProperties rpcTwoConfigurationProperties;
+    private final Executor threadPoolExecutor;
 
     public WebServiceController(WebServiceRpcOneConfigurationProperties rpcOneConfigurationProperties,
-                                WebServiceRpcTwoConfigurationProperties rpcTwoConfigurationProperties) {
+                                WebServiceRpcTwoConfigurationProperties rpcTwoConfigurationProperties,
+                                Executor threadPoolExecutor) {
         this.rpcOneConfigurationProperties = rpcOneConfigurationProperties;
         this.rpcTwoConfigurationProperties = rpcTwoConfigurationProperties;
+        this.threadPoolExecutor = threadPoolExecutor;
     }
 
     @GetMapping("/hello")
@@ -41,7 +49,34 @@ public class WebServiceController {
         ListenableFuture<HelloResponse> response1 = rpcHelloServiceClient1.sayHelloAsync();
         ListenableFuture<HelloResponse> response2 = rpcHelloServiceClient2.sayHelloAsync();
 
-        return new MessagesResponse(Arrays.asList(response1.get().getMessage(),
-                                                  response2.get().getMessage()));
+        CompletableFuture<MessageResponse> result1 = new CompletableFuture<>();
+        Futures.addCallback(response1, new FutureCallback<HelloResponse>() {
+            @Override
+            public void onSuccess(@NullableDecl HelloResponse helloResponse) {
+                result1.complete(new MessageResponse(helloResponse != null ? helloResponse.getMessage() : null));
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                result1.completeExceptionally(throwable);
+            }
+        }, threadPoolExecutor);
+
+        CompletableFuture<MessageResponse> result2 = new CompletableFuture<>();
+        Futures.addCallback(response2, new FutureCallback<HelloResponse>() {
+            @Override
+            public void onSuccess(@NullableDecl HelloResponse helloResponse) {
+                result2.complete(new MessageResponse(helloResponse != null ? helloResponse.getMessage() : null));
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                result2.completeExceptionally(throwable);
+            }
+        }, threadPoolExecutor);
+
+        CompletableFuture.allOf(result1, result2).join();
+
+        return new MessagesResponse(Arrays.asList(result1.get().getMessage(), result2.get().getMessage()));
     }
 }
